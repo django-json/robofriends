3d16de1ef05573d8f4fa477281f6d4420ff67fe2
django-json <diango.jayson@gmail.com>
2019-12-05 09:48:02 +0800

    Updates

---
+++ precache-manifest.74a5a8c86cf311038503192a41ba8181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5502f45051b17133114cb882252aaab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e745d4a15ac7f804b89f",
+    "url": "/static/css/main.ea6e7134.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "e745d4a15ac7f804b89f",
+    "url": "/static/js/main.294e34b2.chunk.js"
+  },
+  {
+    "revision": "c332e1bb67579b93fb1f",
+    "url": "/static/js/runtime-main.b75238ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
